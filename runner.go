22ef9be988095b084463646d16bc6e928d4df6ef
Angel del Castillo <adelcastillo@palo-it.com>
2018-04-03 10:50:56 -0500

    Added runner without tests.

--- runner.go
+++ runner.go
@@ -1,20 +1,39 @@
 package openapitester
 
 import (
+	"bytes"
+	"fmt"
 	"log"
 	"net/http"
 )
 
-func defaultDo(c *http.Client) ([]byte, error) {
-	req, err := http.Request()
-	return nil, nil
+func defaultDo(c *http.Client, method, uri, payload string) ([]byte, int, error) {
+	req, err := http.NewRequest(method, uri, bytes.NewBufferString(payload))
+	if err != nil {
+		return nil, 0, err
+	}
+	resp, err := c.Do(req)
+	if err != nil {
+		return nil, 0, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, 0, fmt.Errorf("invalid response: %d", resp.StatusCode)
+	}
+	buf := new(bytes.Buffer)
+	if _, err := buf.ReadFrom(resp.Body); err != nil {
+		return nil, 0, err
+	}
+	if err := resp.Body.Close(); err != nil {
+		return nil, 0, err
+	}
+	return buf.Bytes(), 0, nil
 }
 
 // Runner type.
 type Runner struct {
 	API    *API
 	Client *http.Client
-	DoFn   func(*http.Client) ([]byte, error)
+	DoFn   func(c *http.Client, method, uri, payload string) ([]byte, int, error)
 }
 
 // NewRunner returns a new runner that consumes oapi.
@@ -27,21 +46,38 @@ func NewRunner(oapi *API, client *http.Client, fnMap map[string]func(string) str
 	return ru, nil
 }
 
+// Report type.
+type Report struct {
+	Code     int
+	Payload  []byte
+	Response []byte
+}
+
 // Exec runs a query against every endpoint registered.
-func (r *Runner) Exec() error {
+func (r *Runner) Exec() ([]*Report, error) {
+	var list []*Report
 	for uri, pats := range r.API.Paths {
 		log.Printf("Exec : %s", uri)
 		for method, pat := range pats {
-			payload, err := r.API.Examples(method, uri)
+			_ = pat
+			examples, err := r.API.Examples(method, uri)
 			if err != nil {
-				return err
+				return nil, err
 			}
-			res, err := r.DoFn(r.client, method, uri, payload)
+			payload := examples[0]
+			res, code, err := r.DoFn(r.Client, method, uri, payload)
 			if err != nil {
-				return err
+				return nil, err
 			}
 			log.Printf("Exec : res : %s", res)
+
+			re := &Report{
+				Code:     code,
+				Payload:  []byte(payload),
+				Response: res,
+			}
+			list = append(list, re)
 		}
 	}
-	return nil
+	return list, nil
 }
